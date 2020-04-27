56b04c1cc2f4ad1d6ee8106a023e8cf84509558f
Anas Ahmad,Anas A <a.anasahmad@student.fontys.nl>
2020-04-27 16:43:20 +0200

    Revert "MVC updated"
    This reverts commit 89cd8e0043d9a73cb90c4d00621d476dcd29f038

--- wad/View/adminIndex.php
+++ wad/View/adminIndex.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="View/CSS/main.css">
+        <link rel="stylesheet" type="text/css" href="CSS/main.css">
         <title>Forum</title>
         
     </head>
@@ -14,7 +14,7 @@
                 <div class="container">
                     <ul>
                         <li class="test"><a><div id="img_prof"></div></a></li>
-                        <li class="test" id="Username_a"><a href="login.php">Username</a></li>
+                        <li class="test" id="Username_a"><a href="">Admin</a></li>
                         <li class="test" style="float: left; padding: 0;"><div id="logo_header"></div></li>
                     </ul>
                 </div>
@@ -23,7 +23,7 @@
         <main>
             <div class="container">
                 <div class="main_info">
-                    <div class="post"><a href="View/topic1.php"> 
+                    <div class="post"><a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
@@ -31,7 +31,7 @@
                         <br></a>
                     </div>
                     <div class="post">
-                    <a href="View/topic1.php"> 
+                    <a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
@@ -39,7 +39,7 @@
                         <br></a>
                     </div>
                     <div class="post">
-                    <a href="View/topic1.php"> 
+                    <a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
@@ -47,7 +47,7 @@
                         <br></a>
                     </div>
                     <div class="post">
-                    <a href="View/topic1.php"> 
+                    <a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
@@ -57,7 +57,7 @@
                 </div>
                 <div class="sidebar">
                     <div class="side_post"> 
-                    <a href="View/topic1.php"> 
+                    <a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
@@ -65,7 +65,7 @@
                         <br></a>
                     </div>
                     <div class="side_post"> 
-                    <a href="View/topic1.php"> 
+                    <a href="adminTopic1.php"> 
                         <br>
                         <h2 class="post_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
                         <br>
